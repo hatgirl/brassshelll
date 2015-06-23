dcbdda999bab78e3a91929eb076428820ea4ffe5
hatgirl <jls6190@rit.edu>
2015-06-23 13:57:14 -0400

    Now forks and waits

--- brassshelll.c
+++ brassshelll.c
@@ -8,15 +8,36 @@ int main(){
   while (1) {
     char * line = NULL;
     size_t sz = 0;
+    printf ("Do stuff? ");
     getline( &line, &sz, stdin);
-    
+    printf ("An excellent choice sire! \n");
+
     cmd_struct* to_invoke = parse_command (line);
     char* prog = to_invoke->progname;
     char** args = to_invoke->args;
     int* redirect = to_invoke->redirect;
-    execvp (prog, args);
-  }
 
+    int ind;
+    int length = sizeof (args) / sizeof(args[0]);
+    for (ind = 0; ind < length; ind++){
+      if (strcmp ("|", args[ind]) == 0){
+	//do special things
+      }
+    }
+
+    if (strcmp("cd", prog) == 0){
+      chdir(args[1]);
+    }
+    else {
+      int pid = fork();
+      if ( pid == 0 ){
+	execvp (prog, args);
+      } else{
+        wait(0);
+      }
+    }
+    
+  }
 
   return 0;
 }
